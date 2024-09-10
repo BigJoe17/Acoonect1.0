9414bc6d0a104874fb7908808cd82ce7dd6b3a81
unknown <forwardeverconsults23@gmail.com>
2024-09-11 00:26:46 +0530

    hello server

---
+++ Client/src/App.tsx
@@ -0,0 +1,16 @@
+function App(){
+  return (
+    <>
+     <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <div>
+      <h1>Hello World👋</h1>
+    </div>
+    <div>
+      <p>Its a connecting with you</p>
+    </div>
+    </>
+  )
+}
+export default App
