4ba40a6f2080f3da18847983feb7822b14028d3b
brunoguyy12 <guybrunondikumana@gmail.com>
2024-09-14 13:27:52 +0530

    extra

--- Client/src/components/Sections/Heropage.tsx
+++ Client/src/components/Sections/Heropage.tsx
@@ -8,7 +8,7 @@ const Heropage = () => {
       <div className="bg-[#f1f1f1]">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-            <div className="flex flex-col justify-center">
+            <div className="flex flex-col justify-center ">
               <h1 className="text-4xl font-inter font-bold text-[var(--foreground)]">
                 Connect, Collaborate, and Thrive in Your Academic Journey
               </h1>
@@ -19,14 +19,16 @@ const Heropage = () => {
                 from your university and beyond
               </p>
               <Link to="/login" className="mt-4">
-                <Button variant="default">Get Started</Button>
+                <Button variant="default" className="!px-8 !py-3">
+                  Get Started
+                </Button>
               </Link>
             </div>
-            <div className="flex justify-center">
+            <div className="flex justify-start md:justify-center ">
               <img
                 src={imagesArray[0]}
                 alt="MIT"
-                className="w-[80%] rounded-lg"
+                className="w-full md:w-[80%] rounded-lg"
               />
             </div>
           </div>
