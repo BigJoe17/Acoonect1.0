5c52b890261011f3e4fe8840ff80ed33e59cd3be
brunoguyy12 <guybrunondikumana@gmail.com>
2024-09-14 23:53:05 +0530

    Good Night

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -3,22 +3,13 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
-import { ClerkProvider } from "@clerk/clerk-react";
 import { Toaster } from "./components/ui/toaster.tsx";
-
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-
-if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key");
-}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Toaster />
-      <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-        <App />
-      </ClerkProvider>
+      <App />
     </BrowserRouter>
   </StrictMode>
 );
